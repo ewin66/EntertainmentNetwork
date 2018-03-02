c587dfc15d7589d0f7781b64aebf4eae8dc3f42f
pbabisd <36399791+pbabisd@users.noreply.github.com>
2018-03-02 12:35:07 +0200

    EntertainmentNetworkClient draft solution has been added.
    EntertainmentNetworkClient draft solution has been added.
    - Server references have been added.
    - Gui modules have been added.

---
+++ EntertainmentNetworkClient/EntertainmentNetworkClient/Modules/Sits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using DevExpress.XtraLayout.Helpers;
+using DevExpress.XtraLayout;
+
+namespace EntertainmentNetworkClient.Modules
+{
+    public partial class Sits : BaseModule
+    {
+        public Sits()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EntertainmentNetworkClient/EntertainmentNetworkClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using EntertainmentNetworkClient.CityService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntertainmentNetworkClient
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new EntertainmentNetwork());
+        }
+    }
+}
